641480dd7f41fd20d4ac0bdef5500e133807016a
okkotsu <st11a6930@gmail.com>
2026-08-24 03:46:34 +0530

    feat: add shorten and redirect endpoints

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from database import create_db_and_tables
 from models import URLs
+from sqlmodel import select
 from contextlib import asynccontextmanager
 from pydantic import BaseModel, HttpUrl
+from database import SessionDep 
+from utils import generate_short_code
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
@@ -19,7 +24,26 @@ def root():
     return {"status" : "alive"}
 
 
-@app.post("/shorten/")
-def shorten():
+@app.post("/shorten/", status_code=201)
+def shorten(request : URLRequest, db : SessionDep):
+    url = URLs(original_url = str(request.original_url), short_code = generate_short_code(6))
+    db.add(url)
+    db.commit()
+    db.refresh(url)
+    return{
+        "short_code":url.short_code,
+        "short_url":"http://localhost:8000/" + url.short_code 
+    }
 
+@app.get("/{short_code}", status_code = 302)
+def redirect(short_code : str, db : SessionDep):
+    statement = select(URLs).where(URLs.short_code == short_code)
+    result = db.exec(statement).first()
+    if result == None:
+        raise HTTPException(
+            status_code = 404,
+            detail = f"{short_code} not found."
+        )
+    else:
+        return RedirectResponse(url = result.original_url)
     
